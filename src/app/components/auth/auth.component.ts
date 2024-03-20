3627554166d885235b627a3b982054be06118c3f
Yifan Zhu <zhuyi@tcd.ie>
2024-03-20 23:29:29 +0000

    Change from Angular Material to Tailwind CSS

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,40 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-auth',
   standalone: true,
   imports: [
     CommonModule,
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule
-],
+    ReactiveFormsModule
+  ],
   templateUrl: './auth.component.html',
-  styleUrl: './auth.component.scss'
+  styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
   authForm!: FormGroup;
   isLoginMode = true;
   isLoggedIn = false;
 
-  constructor(private formBuilder: FormBuilder) {
-    this.authForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-    });}
-
-    toggleMode(event: Event): void {
-      event.preventDefault();
-      this.isLoginMode = !this.isLoginMode;
-    
-      this.authForm.reset();
-    }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
     this.authForm = this.formBuilder.group({
@@ -43,16 +26,15 @@ export class AuthComponent implements OnInit {
     });
   }
 
+  toggleMode(event: Event): void {
+    event.preventDefault();
+    this.isLoginMode = !this.isLoginMode;
+    this.authForm.reset();
+  }
+
   onSubmit(): void {
     if (this.authForm.valid) {
-      if (this.isLoginMode) {
-        // Call login service
-        console.log('Logging in...', this.authForm.value);
-      } else {
-        // Call signup service
-        console.log('Signing up...', this.authForm.value);
-      }
-    
+      console.log(this.isLoginMode ? 'Logging in...' : 'Signing up...', this.authForm.value);
       this.authForm.reset();
     } else {
       console.error('Form is not valid', this.authForm.value);
