394bdd734b031e3175f550b8a408429c1bdd1337
Yifan Zhu <zhuyi@tcd.ie>
2024-04-11 16:31:51 +0100

    Finish user genre page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -42,5 +42,8 @@ export const routes: Routes = [
     }, {
         path: 'wishlist',
         component: WishlistComponent
+    }, {
+        path: 'user-genres',
+        component: UserGenresComponent
     }
 ];
